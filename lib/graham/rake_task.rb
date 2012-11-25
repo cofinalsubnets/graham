da01160d0aacf780246c87ccc95970158264be22
feivel <feivel@sdf.org>
2012-11-25 12:55:35 -0500

    timed rake task for each test group

--- lib/graham/rake_task.rb
+++ lib/graham/rake_task.rb
@@ -1,4 +1,21 @@
 require 'graham'
+# Rake task generator for Graham. Usage is just:
+#  # in Rakefile
+#  require 'graham/rake_task'
+#  Graham::RakeTask.new( options )
+# Options (all optional) are:
+# :dir:: The directory (relative to the Rakefile) where Graham should look for
+#        tests. Default is 'test'
+# :name:: The namespace for generated Rake tasks. Default is 'test'
+# :ignore:: Directories under the test directory that will be ignored for the
+#           purpose of generating namespaced Rake tasks.
+# Let _name_ be the chosen namespace for tests. Then this will
+# 1. Create the task _name_:_folder_ for each subfolder _folder_ under the
+#    testing directory, which (non-recursively) loads each file in _folder_;
+# 2. Create the task _name_, which pulls in all tasks created by (1);
+# 3. For each task _t_ created by (1) and (2), create the task _t_:timed that 
+#    outputs the running time of _t_ once it finishes.
+#
 class Graham::RakeTask
   include Rake::DSL
   def initialize(opts = {})
@@ -8,6 +25,7 @@ class Graham::RakeTask
     make_task
   end
 
+  private
   def make_task
     tests = []
     namespace @name do
@@ -19,7 +37,6 @@ class Graham::RakeTask
           puts "Tests completed after #{Time.now - @start} seconds."
         end
       end
-      task timed: [ "#{@name}:timed:start", @name, "#{@name}:timed:stop" ]
       test_groups.each do |group|
         tests << name = group.sub(/\//,?:).sub(@dir.to_s,@name.to_s)
         task name.sub(/^#{@name}:/,'') do
@@ -31,6 +48,10 @@ class Graham::RakeTask
       end
     end
     task @name => tests
+    tests.each do |test|
+      task "#{test}:timed" => [ "#{@name}:timed:start", test, "#{@name}:timed:stop" ]
+    end
+    task timed: [ "#{@name}:timed:start", @name, "#{@name}:timed:stop" ]
   end
 
   def test_groups
